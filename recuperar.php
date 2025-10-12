97b348817059ff4f77104689bd5b87d087dcfad5
Jepilot-Git <jefferson_m_jose@yahoo.com.br>
2025-10-11 21:34:18 -0300

    Update recuperar.php

--- recuperar.php
+++ recuperar.php
@@ -3,19 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <title>Recuperar Senha - SISMED</title>
-    <link rel="stylesheet" href="css/style.css?v=2">
+    <link rel="stylesheet" href="../CSS/login.css">
 </head>
 <body>
     <div class="recuperar-container">
         <h1>Recuperar Senha</h1>
         <p class="descricao">Digite seu e-mail cadastrado para receber o link de redefinição.</p>
 
-        <form method="POST" action="processa_recuperacao.php">
+        <form method="POST" action="../Esqueci_senha/processa_recuperacao.php">
             <input type="email" name="email" placeholder="Seu e-mail" required>
             <button type="submit">Enviar link</button>
         </form>
 
-        <a href="index.php">← Voltar para o login</a>
+        <a href="../index.php">← Voltar para o login</a>
     </div>
 </body>
 </html>
